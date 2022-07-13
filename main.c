216f393974d661d9c3d0d2d5a6cbde8e2b850679
katriel araujo <katrielaraujo@gmail.com>
2022-07-13 20:30:27 -0300

    testando matriz e criando git ignore

--- main.c
+++ main.c
@@ -7,18 +7,18 @@ int aleatorio(int tam){
   return (rand() % tam);
 }
 
-void gerarMatriz(matriz m){
+void gerarMatriz(matriz *m){
     int count = 0,linha,coluna;
     for(int i = 0; i < 10;i++){
         for(int j = 0;j < 20;j++)
-            m.mat[i][j] = 0;
+            m->mat[i][j] = 0;
     }
     while(count < 40){
         linha =  aleatorio(10);
         coluna = aleatorio(20);
-        if(m.mat[linha][coluna] == 0){
+        if(m->mat[linha][coluna] == 0){
             count++;
-            m.mat[linha][coluna] = 9;
+            m->mat[linha][coluna] = 9;
         }
     }
 }
@@ -26,8 +26,12 @@ void gerarMatriz(matriz m){
 int main(){
     srand(time(NULL));
     matriz m;
-    gerarMatriz(m);
-    printf("%d\n",m.mat[0][0]);
+    gerarMatriz(&m);
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 20;j++)
+            printf("%d ",m.mat[i][j]);
+        printf("\n");
+    }
 
     return 0;
 }
