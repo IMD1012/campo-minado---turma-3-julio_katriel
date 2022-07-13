6c931113067b6ee93af6f8974dc0672f3fc40a90
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-13 10:02:41 -0300

    estou mandando a função aleatorio no main

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "matriz.h"
 
+int aleatorio(){
+  srand(time(NULL));
+
+  int posi[40], posi2[40];
+  
+  for(int i = 0; i < 40; i++){
+    posi[i] = (rand() % 10) + 1;
+    posi2[i] = (rand() % 20) + 1;
+    
+    printf("[%d][%d] \n",posi[i],posi2[i]);
+  }
+}
+
 int main(){
     matriz m;
     m.mat[0][0] = 1;
