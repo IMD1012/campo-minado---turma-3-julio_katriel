89ab4eb1c31e6a699628baa6618fb752723c4dd6
katriel araujo <katrielaraujo@gmail.com>
2022-07-13 22:32:37 -0300

    alteranção na função como_jogar

--- funcs.c
+++ funcs.c
@@ -12,4 +12,6 @@ void como_jogar(){
     printf("Digite uma letra no intervalor de A a J\n");
     printf("Digite um número no intervalor de 1 à 20\n");
     printf("Exemplos: a7,j9 e e13\n");
+    printf("Para ver o tempo de jogo digite: t\n");
+    printf("Para pedir ajudar do computador digite: h\n");
 }
