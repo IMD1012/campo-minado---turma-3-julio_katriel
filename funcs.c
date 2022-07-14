db9224595a45b8ef4f04f10740456ac8407178fc
katriel araujo <katrielaraujo@gmail.com>
2022-07-13 22:26:08 -0300

    arquivo funcs e mostrar tela e como_jogar

---
+++ funcs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "matriz.h"
+
+void mostrar_tela(matriz m){
+    printf("---------------------------------------------------------\n");
+    printf("                      CAMPO MINADO                       \n");
+    printf("---------------------------------------------------------\n");
+}
+
+void como_jogar(){
+    printf("Digite uma letra no intervalor de A a J\n");
+    printf("Digite um número no intervalor de 1 à 20\n");
+    printf("Exemplos: a7,j9 e e13\n");
+}
