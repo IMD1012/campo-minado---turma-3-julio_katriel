fa6e19d7ae9e3ec51d085ae04b599fd10ff28dc8
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-17 14:44:50 -0300

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -63,6 +63,7 @@ int verifica_vizinho(int l, int c, matriz *m){
 int verifica_aberto(int l, int c, matriz *m){
   return (coordenada_valida(l,c) && (m->mat[l][c].visibilidade == 0));
 }
+
 //Em produção
 int abre_vizinho(int l, int c, matriz *m){
   int x;
@@ -115,16 +116,6 @@ int abre_celula(matriz m, matriz m2){
         y = convert_char(x);
         m2.mat[l][c].caractere = y;
       }
-      
-      if(m2.mat[l][c].caractere == '0'){
-        abre_vizinho(l,c,&m2);
-      }
       count++;
     }while(count != 160);
-}
-
-void mostrar_tela(matriz m){
-    printf("---------------------------------------------------------\n");
-    printf("                      CAMPO MINADO                       \n");
-    printf("---------------------------------------------------------\n");
 }
