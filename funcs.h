23676fd3d8ac132d2445de900e75ea6996d23587
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-18 10:05:30 -0300

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,13 +1,14 @@
 #ifndef FUNCS_H_
 #define FUNCS_H_
 
+void trata_coluna(char c, matriz m);
+int trata_linha(int l, matriz m);
 int aleatorio(int tam);
 char convert_char(int n);
-void tela_inicial(matriz m, matriz m2);
+void tela_inicial(matriz m);
 int coordenada_valida(int l, int c);
-int ordena_menos_um(int l1);
 int troca(char letra);
-void como_jogar(matriz m, matriz m2);
+void como_jogar(matriz m);
 void fim_de_jogo();
 
 #endif
