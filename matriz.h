89c054774fff58d40c7f16fb8ccaf85288cae7f5
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-18 15:55:05 -0300

    correção

--- matriz.h
+++ matriz.h
@@ -15,8 +15,8 @@ void gerar_matriz(matriz *m);
 void colocar_bombas(matriz *m);
 int contador_visivel(matriz m);
 void como_jogar(matriz m);
-void tela_inicial(matriz *m)
+void tela_inicial(matriz *m);
 int verificar_arredor(int l, int c, matriz *m);
 int jogo(matriz *m);
-int aleatorio(int tam);
+void trata_coluna(char c, matriz m);
 #endif
