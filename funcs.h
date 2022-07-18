4d268eb9b89cb36abbf8670fde890ede4f6e5faf
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-18 15:55:29 -0300

    correção

--- funcs.h
+++ funcs.h
@@ -1,13 +1,12 @@
 #ifndef FUNCS_H_
 #define FUNCS_H_
 
-void trata_coluna(char c, matriz m);
 int trata_linha(int l);
 int aleatorio(int tam);
 char convert_int(int n);
 int coordenada_valida(int l, int c);
 int convert_char(char letra);
-void entrada(int *l,int *c)
+void entrada(int *l,int *c);
 void fim_de_jogo();
 
 #endif
