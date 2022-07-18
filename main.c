7db1a64180857aa5187ae094a332b12feaf41519
julions27 <109082651+julions27@users.noreply.github.com>
2022-07-18 10:05:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 #include "matriz.h"
 #include "funcs.h"
 
 int main(){
     srand(time(NULL));
     matriz m;
-    matriz m2;
     gerar_Matriz(&m);
     colocar_bombas(&m);
-    gerar_Matriz(&m2);
-    tela_inicial(m,m2);
+    tela_inicial(m);
     return 0;
 }
